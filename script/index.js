08177755542c04167af3edafa4610ff19d380fc3
ovplotnikov <106397201+ovplotnikov@users.noreply.github.com>
2023-01-07 16:40:41 +0600

    layout fixes rev.1

--- script/index.js
+++ script/index.js
@@ -22,24 +22,24 @@ addEventListener('click', (event) => {
 let profile__name = document.querySelector('.profile__name');
 let profile__about = document.querySelector('.profile__about');
 
-let popup__input_name = document.querySelector('.popup__input_name');
-let popup__input_about = document.querySelector('.popup__input_about');
+let popup__form_input_name = document.querySelector('.popup__form_input_name');
+let popup__form_input_about = document.querySelector('.popup__form_input_about');
 
-popup__input_name.value = profile__name.textContent;
-popup__input_about.value = profile__about.textContent;
+popup__form_input_name.value = profile__name.textContent;
+popup__form_input_about.value = profile__about.textContent;
 
 
 
 let formElement = document.querySelector('.popup__form');
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
-    // Получите значение полей .popup__input_about и .popup__input_name из свойства value
+    // Получите значение полей .popup__form_input_about и .popup__form_input_name из свойства value
 
-    let popup__input_name = document.querySelector('.popup__input_name');
-    let popup__input_about = document.querySelector('.popup__input_about');
+    let popup__form_input_name = document.querySelector('.popup__form_input_name');
+    let popup__form_input_about = document.querySelector('.popup__form_input_about');
 
-    profile__name.textContent  = popup__input_name.value;
-    profile__about.textContent = popup__input_about.value;
+    profile__name.textContent  = popup__form_input_name.value;
+    profile__about.textContent = popup__form_input_about.value;
 
     popup.classList.remove('popup_opened');
 }
