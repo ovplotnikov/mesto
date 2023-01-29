d4caf1bfa3fccd24686c963cac1dfb323caa8719
ovplotnikov <106397201+ovplotnikov@users.noreply.github.com>
2023-01-29 20:44:25 +0600

    popup add-card is done

--- script/index.js
+++ script/index.js
@@ -1,25 +1,38 @@
 
-let popup = document.querySelector('.popup');
+let popupEditProfile = document.querySelector('.popup_type_edit-profile');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 let popupFormInputName = document.querySelector('.popup__input_value_name');
 let popupFormInputAbout = document.querySelector('.popup__input_value_about');
+let popupAddCard = document.querySelector('.popup_type_add-card');
+
 
 addEventListener('click', (event) => {
   if (event.target.classList.contains('profile__edit-button')) {
-    popup.classList.add('popup_opened');
+    popupEditProfile.classList.add('popup_opened');
     popupFormInputName.value = profileName.textContent;
     popupFormInputAbout.value = profileAbout.textContent;
   }
 });
 
 addEventListener('click', (event) => {
+  if (event.target.classList.contains('profile__add-button')) {
+    popupAddCard.classList.add('popup_opened');
+  }
+});
+
+addEventListener('click', (event) => {
   if (event.target.classList.contains('popup__close-button')) {
-    popup.classList.remove('popup_opened');
+    popupEditProfile.classList.remove('popup_opened');
+    popupAddCard.classList.remove('popup_opened');
   }
-  else if (event.target.classList.contains('popup')) {
-    popup.classList.remove('popup_opened');
+  else if (event.target.classList.contains('popup_type_edit-profile')) {
+    popupEditProfile.classList.remove('popup_opened');
   }
+  else if (event.target.classList.contains('popup_type_add-card')) {
+    popupAddCard.classList.remove('popup_opened');
+  }
+
 });
 
 let formElement = document.querySelector('.popup__form');
@@ -28,7 +41,7 @@ function handleFormSubmit (evt) {
     profileName.textContent  = popupFormInputName.value;
     profileAbout.textContent = popupFormInputAbout.value;
 
-    popup.classList.remove('popup_opened');
+    popupEditProfile.classList.remove('popup_opened');
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
