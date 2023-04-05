03508ca24462a373758ea79e559a1646edd14715
ovplotnikov <106397201+ovplotnikov@users.noreply.github.com>
2023-04-05 22:06:52 +0600

    mini-css-extract-plugin installed

--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,17 @@ module.exports = {
     open: true
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         use: 'babel-loader',
-        exclude: /node_modules/
-      }
+        exclude: '/node_modules/'
+      },
+      // добавили правило для обработки файлов
+      {
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
     ]
   },
   plugins: [
