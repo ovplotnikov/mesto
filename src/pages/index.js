bc76c124734a8879c90e66c395df8dfe74c0ad72
ovplotnikov <106397201+ovplotnikov@users.noreply.github.com>
2023-04-06 19:40:15 +0600

    webpack ready dir

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 // Imports
-import Card from "./Card.js";
-import FormValidator from "./FormValidator.js";
-import Section from "./Section.js";
-import PopupWithForm from "./PopupWithForm.js";
-import PopupWithImage from './PopupWithImage.js';
-import UserInfo from './UserInfo.js';
-import { validationConfig, initialElements } from "./constants.js";
+import Card from "../components/Card.js";
+import FormValidator from "../components/FormValidator.js";
+import Section from "../components/Section.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWithImage from '../components/PopupWithImage.js';
+import UserInfo from '../components/UserInfo.js';
+import { validationConfig, initialElements } from "../utils/constants.js";
 
 // Create an instance of the UserInfo class
 const userInfo = new UserInfo({
