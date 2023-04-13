f8b82865685d5013a317b8b047e22a6666c48d4f
ovplotnikov <106397201+ovplotnikov@users.noreply.github.com>
2023-04-13 18:15:14 +0600

    like counter added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,56 +1,64 @@
 export default class Card {
-    constructor(data, templateSelector, handleCardClick) {
-        this._name = data.name; 
-        this._link = data.link; 
-        this._templateSelector = templateSelector;
-        this._elementImage = null;
-        this._buttonLike = null;
-        this._handleCardClick = handleCardClick;
-    }
+  constructor(data, templateSelector, handleCardClick) {
+    this._name = data.name;
+    this._link = data.link;
+    this._likes = data.likes;
+    this._templateSelector = templateSelector;
+    this._elementImage = null;
+    this._buttonLike = null;
+    this._handleCardClick = handleCardClick;
+  }
 
-   _getTemplate() {
-        const cardElement = document
-            .querySelector(this._templateSelector)
-            .content
-            .querySelector('.elements__item')
-            .cloneNode(true);
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._templateSelector)
+      .content.querySelector(".elements__item")
+      .cloneNode(true);
 
-        return cardElement;
-    }
+    return cardElement;
+  }
 
-    generateCard() {
-        this._element = this._getTemplate();
-        this._element.querySelector('.elements__title').textContent = this._name;
-        this._elementImage = this._element.querySelector('.elements__image');
-        this._elementImage.src = this._link;
-        this._elementImage.alt = this._name;
-        this._setEventListeners();
+  generateCard() {
+    this._element = this._getTemplate();
+    this._element.querySelector(".elements__title").textContent = this._name;
+    this._elementImage = this._element.querySelector(".elements__image");
+    this._elementImage.src = this._link;
+    this._elementImage.alt = this._name;
+    this._likeCounter = this._element.querySelector(".elements__like-counter");
+    this._setLikesCount(this._likes.length);
+    this._setEventListeners();
 
-        return this._element;
-    }
+    return this._element;
+  }
 
-    _setEventListeners() {
-        this._buttonLike = this._element.querySelector('.elements__like-button');
-        this._buttonLike.addEventListener('click', () => {
-            this._handleLikeCard();
-        });
+  _setLikesCount(count) {
+    this._likeCounter.textContent = count;
+    this._likeCounter.dataset.likes = count;
+  }
 
-        this._element.querySelector('.elements__delete-button').addEventListener('click', () => {
-            this._handleDeleteCard();
-        });
+  _setEventListeners() {
+    this._buttonLike = this._element.querySelector(".elements__like-button");
+    this._buttonLike.addEventListener("click", () => {
+      this._handleLikeCard();
+    });
 
-        this._elementImage.addEventListener('click', () => {
-            this._handleCardClick(this._name, this._link);
-        });
-    }
+    this._element
+      .querySelector(".elements__delete-button")
+      .addEventListener("click", () => {
+        this._handleDeleteCard();
+      });
 
-    _handleLikeCard() {
-        this._buttonLike.classList.toggle('elements__like-button_active');
-    }
-    
+    this._elementImage.addEventListener("click", () => {
+      this._handleCardClick(this._name, this._link);
+    });
+  }
 
-    _handleDeleteCard() {
-        this._element.remove();
-        this._element = null;
-    }
+  _handleLikeCard() {
+    this._buttonLike.classList.toggle("elements__like-button_active");
+  }
+
+  _handleDeleteCard() {
+    this._element.remove();
+    this._element = null;
+  }
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -26,29 +26,29 @@ export const formAddCard = document
   .querySelector(validationConfig.formSelector);
 
 // Массив с первоначальными карточками
-export const initialElements = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
+// export const initialElements = [
+//   {
+//     name: "Архыз",
+//     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+//   },
+//   {
+//     name: "Челябинская область",
+//     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+//   },
+//   {
+//     name: "Иваново",
+//     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+//   },
+//   {
+//     name: "Камчатка",
+//     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+//   },
+//   {
+//     name: "Холмогорский район",
+//     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+//   },
+//   {
+//     name: "Байкал",
+//     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+//   },
+// ];
